1df6cd4c92432f861990a34adeac5c0d5423eda2
Tabitha Johnston <tabithasj@iclud.com>
2020-06-05 15:07:36 +0200

    Initial: setting up repo with minimal contents

---
+++ vector_ops/vectors.py
@@ -0,0 +1,33 @@
+class Vector:
+    """
+    A class describing 2D and 3D vectors and their properties.
+    """
+    def __init__(self, i, j, k):
+        self.i = i
+        self.j = j
+        self.k = k
+
+
+    def dot_product(self, other):
+        """
+        Function which calculates the dot product of two vectors
+
+        INPUTS:
+        self - 2D or 3D vector
+
+        OUTPUTS:
+        dp - a value for the dot product
+        insight - string statement, commenting on the size of the angle between these two vectors
+        """
+        dp = other.i * self.i + other.j * self.j + other.k * self.k
+
+        if dp == 0:
+            insight = "since the dot product is zero, the angle between these two vectors is Pi / 2"
+        elif dp > 0:
+            insight = "since the dot product is positive, the angle between these two vectors is less than Pi / 2"
+        else:
+            insight = "since the dot product is negative, the angle between these two vectors is greater than than Pi / 2"
+
+        return(dp, insight)
+
+    
