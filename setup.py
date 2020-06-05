86ec7626c8cd380ae1a88a8439d73cb4ac37a1dd
Tabitha Johnston <tabithasj@iclud.com>
2020-06-05 15:21:32 +0200

    Docs: added __init__ & setup.py files for vector_ops package

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name = 'vector_ops',
+      version = '0.1',
+      author = 'Tabitha Johnston',
+      url = "https://github.com/SassyData/PyVectorsPkg",
+      packages = ['vector_ops'],
+      zip_safe = False)
